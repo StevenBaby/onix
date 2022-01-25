d8b5d157f12eda486eb9a38592d99c27f1937c81
Steven Kang <kangweibaby@163.com>
2022-01-25 20:56:13 +0800

    002 配置开发环境

---
+++ src/boot.asm
@@ -0,0 +1,27 @@
+[org 0x7c00]
+
+; 设置屏幕模式为文本模式，清除屏幕
+mov ax, 3
+int 0x10
+
+; 初始化段寄存器
+mov ax, 0
+mov ds, ax
+mov es, ax
+mov ss, ax
+mov sp, 0x7c00
+
+; 0xb8000 文本显示器的内存区域
+mov ax, 0xb800
+mov ds, ax
+mov byte [0], 'H'
+
+; 阻塞
+jmp $
+
+; 填充 0
+times 510 - ($ - $$) db 0
+
+; 主引导扇区的最后两个字节必须是 0x55 0xaa
+; dw 0xaa55
+db 0x55, 0xaa
