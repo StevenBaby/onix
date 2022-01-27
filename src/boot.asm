f7c62720207fd0e62769acab688fd65a64437296
Steven Kang <kangweibaby@163.com>
2022-01-27 17:09:28 +0800

    004 在 U 盘启动

--- src/boot.asm
+++ src/boot.asm
@@ -15,6 +15,19 @@ mov sp, 0x7c00
 mov ax, 0xb800
 mov ds, ax
 mov byte [0], 'H'
+mov byte [2], 'e'
+mov byte [4], 'l'
+mov byte [6], 'l'
+mov byte [8], 'o'
+mov byte [10], ' '
+mov byte [12], 'w'
+mov byte [14], 'o'
+mov byte [16], 'r'
+mov byte [18], 'l'
+mov byte [20], 'd'
+mov byte [22], '!'
+mov byte [24], '!'
+mov byte [26], '!'
 
 ; 阻塞
 jmp $
