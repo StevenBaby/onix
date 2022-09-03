3a4b37c1371c4c779eb87a8dd5c9694352c20207
Steven Kang <kangweibaby@163.com>
2022-09-03 20:37:45 +0800

    ✨ 077 硬盘分区

--- src/include/onix/ide.h
+++ src/include/onix/ide.h
@@ -8,18 +8,50 @@
 
 #define IDE_CTRL_NR 2 // 控制器数量，固定为 2
 #define IDE_DISK_NR 2 // 每个控制器可挂磁盘数量，固定为 2
+#define IDE_PART_NR 4 // 每个磁盘分区数量，只支持主分区，总共 4 个
+
+typedef struct part_entry_t
+{
+    u8 bootable;             // 引导标志
+    u8 start_head;           // 分区起始磁头号
+    u8 start_sector : 6;     // 分区起始扇区号
+    u16 start_cylinder : 10; // 分区起始柱面号
+    u8 system;               // 分区类型字节
+    u8 end_head;             // 分区的结束磁头号
+    u8 end_sector : 6;       // 分区结束扇区号
+    u16 end_cylinder : 10;   // 分区结束柱面号
+    u32 start;               // 分区起始物理扇区号 LBA
+    u32 count;               // 分区占用的扇区数
+} _packed part_entry_t;
+
+typedef struct boot_sector_t
+{
+    u8 code[446];
+    part_entry_t entry[4];
+    u16 signature;
+} _packed boot_sector_t;
+
+typedef struct ide_part_t
+{
+    char name[8];            // 分区名称
+    struct ide_disk_t *disk; // 磁盘指针
+    u32 system;              // 分区类型
+    u32 start;               // 分区起始物理扇区号 LBA
+    u32 count;               // 分区占用的扇区数
+} ide_part_t;
 
 // IDE 磁盘
 typedef struct ide_disk_t
 {
-    char name[8];            // 磁盘名称
-    struct ide_ctrl_t *ctrl; // 控制器指针
-    u8 selector;             // 磁盘选择
-    bool master;             // 主盘
-    u32 total_lba;           // 可用扇区数量
-    u32 cylinders;           // 柱面数
-    u32 heads;               // 磁头数
-    u32 sectors;             // 扇区数
+    char name[8];                  // 磁盘名称
+    struct ide_ctrl_t *ctrl;       // 控制器指针
+    u8 selector;                   // 磁盘选择
+    bool master;                   // 主盘
+    u32 total_lba;                 // 可用扇区数量
+    u32 cylinders;                 // 柱面数
+    u32 heads;                     // 磁头数
+    u32 sectors;                   // 扇区数
+    ide_part_t parts[IDE_PART_NR]; // 硬盘分区
 } ide_disk_t;
 
 // IDE 控制器
--- src/kernel/ide.c
+++ src/kernel/ide.c
@@ -65,6 +65,16 @@
 #define IDE_LBA_MASTER 0b11100000 // 主盘 LBA
 #define IDE_LBA_SLAVE 0b11110000  // 从盘 LBA
 
+// 分区文件系统
+// 参考 https://www.win.tue.nl/~aeb/partitions/partition_types-1.html
+typedef enum PART_FS
+{
+    PART_FS_FAT12 = 1,    // FAT12
+    PART_FS_EXTENDED = 5, // 扩展分区
+    PART_FS_MINIX = 0x80, // minux
+    PART_FS_LINUX = 0x83, // linux
+} PART_FS;
+
 typedef struct ide_params_t
 {
     u16 config;                 // 0 General configuration bits
@@ -304,6 +314,18 @@ int ide_pio_write(ide_disk_t *disk, void *buf, u8 count, idx_t lba)
     return 0;
 }
 
+// 读分区
+int ide_pio_part_read(ide_part_t *part, void *buf, u8 count, idx_t lba)
+{
+    return ide_pio_read(part->disk, buf, count, part->start + lba);
+}
+
+// 写分区
+int ide_pio_part_write(ide_part_t *part, void *buf, u8 count, idx_t lba)
+{
+    return ide_pio_write(part->disk, buf, count, part->start + lba);
+}
+
 static void ide_swap_pairs(char *buf, u32 len)
 {
     for (size_t i = 0; i < len; i += 2)
@@ -359,6 +381,60 @@ rollback:
     return ret;
 }
 
+static void ide_part_init(ide_disk_t *disk, u16 *buf)
+{
+    // 磁盘不可用
+    if (!disk->total_lba)
+        return;
+
+    // 读取主引导扇区
+    ide_pio_read(disk, buf, 1, 0);
+
+    // 初始化主引导扇区
+    boot_sector_t *boot = (boot_sector_t *)buf;
+
+    for (size_t i = 0; i < IDE_PART_NR; i++)
+    {
+        part_entry_t *entry = &boot->entry[i];
+        ide_part_t *part = &disk->parts[i];
+        if (!entry->count)
+            continue;
+
+        sprintf(part->name, "%s%d", disk->name, i + 1);
+
+        LOGK("part %s \n", part->name);
+        LOGK("    bootable %d\n", entry->bootable);
+        LOGK("    start %d\n", entry->start);
+        LOGK("    count %d\n", entry->count);
+        LOGK("    system 0x%x\n", entry->system);
+
+        part->disk = disk;
+        part->count = entry->count;
+        part->system = entry->system;
+        part->start = entry->start;
+
+        if (entry->system == PART_FS_EXTENDED)
+        {
+            LOGK("Unsupported extended partition!!!\n");
+
+            boot_sector_t *eboot = (boot_sector_t *)(buf + SECTOR_SIZE);
+            ide_pio_read(disk, (void *)eboot, 1, entry->start);
+
+            for (size_t j = 0; j < IDE_PART_NR; j++)
+            {
+                part_entry_t *eentry = &eboot->entry[j];
+                if (!eentry->count)
+                    continue;
+                LOGK("part %d extend %d\n", i, j);
+                LOGK("    bootable %d\n", eentry->bootable);
+                LOGK("    start %d\n", eentry->start);
+                LOGK("    count %d\n", eentry->count);
+                LOGK("    system 0x%x\n", eentry->system);
+            }
+        }
+    }
+}
+
 // ide 控制器初始化
 static void ide_ctrl_init()
 {
@@ -398,6 +474,7 @@ static void ide_ctrl_init()
                 disk->selector = IDE_LBA_MASTER;
             }
             ide_identify(disk, buf);
+            ide_part_init(disk, buf);
         }
     }
     free_kpage((u32)buf, 1);
