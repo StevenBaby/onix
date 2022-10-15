2f57e64457bd861171fe686f2e38dd450b218aed
Steven Kang <kangweibaby@163.com>
2022-10-15 21:36:56 +0800

    ✨ 084 根超级块

--- src/fs/super.c
+++ src/fs/super.c
@@ -7,52 +7,114 @@
 
 #define LOGK(fmt, args...) DEBUGK(fmt, ##args)
 
-void super_init()
+#define SUPER_NR 16
+
+static super_block_t super_table[SUPER_NR]; // 超级块表
+static super_block_t *root;                 // 根文件系统超级块
+
+// 从超级块表中查找一个空闲块
+static super_block_t *get_free_super()
 {
+    for (size_t i = 0; i < SUPER_NR; i++)
+    {
+        super_block_t *sb = &super_table[i];
+        if (sb->dev == EOF)
+        {
+            return sb;
+        }
+    }
+    panic("no more super block!!!");
+}
+
+// 获得设备 dev 的超级块
+super_block_t *get_super(dev_t dev)
+{
+    for (size_t i = 0; i < SUPER_NR; i++)
+    {
+        super_block_t *sb = &super_table[i];
+        if (sb->dev == dev)
+        {
+            return sb;
+        }
+    }
+    return NULL;
+}
+
+// 读设备 dev 的超级块
+super_block_t *read_super(dev_t dev)
+{
+    super_block_t *sb = get_super(dev);
+    if (sb)
+    {
+        return sb;
+    }
+
+    LOGK("Reading super block of device %d\n", dev);
+
+    // 获得空闲超级块
+    sb = get_free_super();
+
+    // 读取超级块
+    buffer_t *buf = bread(dev, 1);
+
+    sb->buf = buf;
+    sb->desc = (super_desc_t *)buf->data;
+    sb->dev = dev;
+
+    assert(sb->desc->magic == MINIX1_MAGIC);
+
+    memset(sb->imaps, 0, sizeof(sb->imaps));
+    memset(sb->zmaps, 0, sizeof(sb->zmaps));
+
+    // 读取 inode 位图
+    int idx = 2; // 块位图从第 2 块开始，第 0 块 引导块，第 1 块 超级块
+
+    for (int i = 0; i < sb->desc->imap_blocks; i++)
+    {
+        assert(i < IMAP_NR);
+        if ((sb->imaps[i] = bread(dev, idx)))
+            idx++;
+        else
+            break;
+    }
+
+    // 读取块位图
+    for (int i = 0; i < sb->desc->zmap_blocks; i++)
+    {
+        assert(i < ZMAP_NR);
+        if ((sb->zmaps[i] = bread(dev, idx)))
+            idx++;
+        else
+            break;
+    }
+
+    return sb;
+}
+
+// 挂载根文件系统
+static void mount_root()
+{
+    LOGK("Mount root file system...\n");
+    // 假设主硬盘第一个分区是根文件系统
     device_t *device = device_find(DEV_IDE_PART, 0);
     assert(device);
 
-    buffer_t *boot = bread(device->dev, 0);
-    buffer_t *super = bread(device->dev, 1);
+    // 读更文件系统超级块
+    root = read_super(device->dev);
+}
 
-    super_desc_t *sb = (super_desc_t *)super->data;
-    assert(sb->magic == MINIX1_MAGIC);
-
-    // inode 位图
-    buffer_t *imap = bread(device->dev, 2);
-
-    // 块位图
-    buffer_t *zmap = bread(device->dev, 2 + sb->imap_blocks);
-
-    // 读取第一个 inode 块
-    buffer_t *buf1 = bread(device->dev, 2 + sb->imap_blocks + sb->zmap_blocks);
-    inode_desc_t *inode = (inode_desc_t *)buf1->data;
-
-    buffer_t *buf2 = bread(device->dev, inode->zone[0]);
-
-    dentry_t *dir = (dentry_t *)buf2->data;
-    inode_desc_t *helloi = NULL;
-    while (dir->nr)
+void super_init()
+{
+    for (size_t i = 0; i < SUPER_NR; i++)
     {
-        LOGK("inode %04d, name %s\n", dir->nr, dir->name);
-        if (!strcmp(dir->name, "hello.txt"))
-        {
-            helloi = &((inode_desc_t *) buf1->data)[dir->nr - 1];
-            strcpy(dir->name, "world.txt");
-            buf2->dirty = true;
-            bwrite(buf2);
-        }
-        dir++;
+        super_block_t *sb = &super_table[i];
+        sb->dev = EOF;
+        sb->desc = NULL;
+        sb->buf = NULL;
+        sb->iroot = NULL;
+        sb->imount = NULL;
+        list_init(&sb->inode_list);
     }
 
-    buffer_t *buf3 = bread(device->dev, helloi->zone[0]);
-    LOGK("content %s", buf3->data);
-
-    strcpy(buf3->data, "This is modified content!!!\n");
-    buf3->dirty = true;
-    bwrite(buf3);
-
-    helloi->size = strlen(buf3->data);
-    buf1->dirty = true;
-    bwrite(buf1);
+    mount_root();
 }
--- src/include/onix/fs.h
+++ src/include/onix/fs.h
@@ -8,10 +8,10 @@
 #define SECTOR_SIZE 512 // 扇区大小
 
 #define MINIX1_MAGIC 0x137F // 文件系统魔数
-#define NAME_LEN 14        // 文件名长度
+#define NAME_LEN 14         // 文件名长度
 
-#define IMAP_NR 8
-#define ZMAP_NR 8
+#define IMAP_NR 8 // inode 位图块，最大值
+#define ZMAP_NR 8 // 块位图块，最大值
 
 typedef struct inode_desc_t
 {
@@ -23,6 +23,19 @@ typedef struct inode_desc_t
     u8 nlinks;   // 链接数（多少个文件目录项指向该i 节点）
     u16 zone[9]; // 直接 (0-6)、间接(7)或双重间接 (8) 逻辑块号
 } inode_desc_t;
+
+typedef struct inode_t
+{
+    inode_desc_t *desc;   // inode 描述符
+    struct buffer_t *buf; // inode 描述符对应 buffer
+    dev_t dev;            // 设备号
+    idx_t nr;             // i 节点号
+    u32 count;            // 引用计数
+    time_t atime;         // 访问时间
+    time_t ctime;         // 创建时间
+    list_node_t node;     // 链表结点
+    dev_t mount;          // 安装设备
+} inode_t;
 
 typedef struct super_desc_t
 {
@@ -36,11 +49,26 @@ typedef struct super_desc_t
     u16 magic;         // 文件系统魔数
 } super_desc_t;
 
+typedef struct super_block_t
+{
+    super_desc_t *desc;              // 超级块描述符
+    struct buffer_t *buf;            // 超级快描述符 buffer
+    struct buffer_t *imaps[IMAP_NR]; // inode 位图缓冲
+    struct buffer_t *zmaps[ZMAP_NR]; // 块位图缓冲
+    dev_t dev;                       // 设备号
+    list_t inode_list;               // 使用中 inode 链表
+    inode_t *iroot;                  // 根目录 inode
+    inode_t *imount;                 // 安装到的 inode
+} super_block_t;
+
 // 文件目录项结构
 typedef struct dentry_t
 {
     u16 nr;              // i 节点
     char name[NAME_LEN]; // 文件名
 } dentry_t;
+
+super_block_t *get_super(dev_t dev);  // 获得 dev 对应的超级块
+super_block_t *read_super(dev_t dev); // 读取 dev 对应的超级块
 
 #endif
