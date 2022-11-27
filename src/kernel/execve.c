8b88a586dfd8cfadbf15e779d17952937f6bd988
Steven Kang <kangweibaby@163.com>
2022-11-27 15:37:39 +0800

    ✨ 109 ELF 符号解析

--- src/kernel/execve.c
+++ src/kernel/execve.c
@@ -114,7 +114,7 @@ enum SegmentFlag
 
 typedef struct Elf32_Shdr
 {
-    Elf32_Word sh_name;      // 节名
+    Elf32_Word sh_name;      // 节名，在字符串表中的索引
     Elf32_Word sh_type;      // 节类型，见 SectionType
     Elf32_Word sh_flags;     // 节标记，见 SectionFlag
     Elf32_Addr sh_addr;      // 节地址
@@ -125,6 +125,74 @@ typedef struct Elf32_Shdr
     Elf32_Word sh_addralign; // 地址对齐约束
     Elf32_Word sh_entsize;   // 子项入口大小
 } Elf32_Shdr;
+
+enum SectionType
+{
+    SHT_NULL = 0,            // 不可用
+    SHT_PROGBITS = 1,        // 程序信息
+    SHT_SYMTAB = 2,          // 符号表
+    SHT_STRTAB = 3,          // 字符串表
+    SHT_RELA = 4,            // 有附加重定位
+    SHT_HASH = 5,            // 符号哈希表
+    SHT_DYNAMIC = 6,         // 动态链接信息
+    SHT_NOTE = 7,            // 标记文件信息
+    SHT_NOBITS = 8,          // 该节文件中无内容
+    SHT_REL = 9,             // 无附加重定位
+    SHT_SHLIB = 10,          // 保留，用于非特定的语义
+    SHT_DYNSYM = 11,         // 符号表
+    SHT_LOPROC = 0x70000000, // 以下与处理器相关
+    SHT_HIPROC = 0x7fffffff,
+    SHT_LOUSER = 0x80000000,
+    SHT_HIUSER = 0xffffffff,
+};
+
+enum SectionFlag
+{
+    SHF_WRITE = 0x1,           // 执行时可写
+    SHF_ALLOC = 0x2,           // 执行时占用内存，有些节执行时可以不在内存中
+    SHF_EXECINSTR = 0x4,       // 包含可执行的机器指令，节里有代码
+    SHF_MASKPROC = 0xf0000000, // 保留，与处理器相关
+};
+
+typedef struct Elf32_Sym
+{
+    Elf32_Word st_name;  // 符号名称，在字符串表中的索引
+    Elf32_Addr st_value; // 符号值，与具体符号相关
+    Elf32_Word st_size;  // 符号的大小
+    u8 st_info;          // 指定符号类型和约束属性，见 SymbolBinding
+    u8 st_other;         // 为 0，无意义
+    Elf32_Half st_shndx; // 符号对应的节表索引
+} Elf32_Sym;
+
+// 通过 info 获取约束
+#define ELF32_ST_BIND(i) ((i) >> 4)
+// 通过 info 获取类型
+#define ELF32_ST_TYPE(i) ((i)&0xF)
+// 通过 约束 b 和 类型 t 获取 info
+#define ELF32_ST_INFO(b, t) (((b) << 4) + ((t)&0xf))
+
+// 符号约束
+enum SymbolBinding
+{
+    STB_LOCAL = 0,   // 外部不可见符号，优先级最高
+    STB_GLOBAL = 1,  // 外部可见符号
+    STB_WEAK = 2,    // 弱符号，外部可见，如果符号重定义，则优先级更低
+    STB_LOPROC = 13, // 处理器相关低位
+    STB_HIPROC = 15, // 处理器相关高位
+};
+
+// 符号类型
+enum SymbolType
+{
+    STT_NOTYPE = 0,  // 未定义
+    STT_OBJECT = 1,  // 数据对象，比如 变量，数组等
+    STT_FUNC = 2,    // 函数或可执行代码
+    STT_SECTION = 3, // 节，用于重定位
+    STT_FILE = 4,    // 文件，节索引为 SHN_ABS，约束为 STB_LOCAL，
+                     // 而且优先级高于其他 STB_LOCAL 符号
+    STT_LOPROC = 13, // 处理器相关
+    STT_HIPROC = 15, // 处理器相关
+};
 
 int sys_execve(char *filename, char *argv[], char *envp[])
 {
@@ -169,8 +237,85 @@ int sys_execve(char *filename, char *argv[], char *envp[])
         ptr++;
     }
 
+    // 节头
+    Elf32_Shdr *shdr = (Elf32_Shdr *)alloc_kpage(1);
+    lseek(fd, ehdr->e_shoff, SEEK_SET);
+    read(fd, (char *)shdr, ehdr->e_shentsize * ehdr->e_shnum);
+    LOGK("ELF section size mem %d\n", ehdr->e_shentsize * ehdr->e_shnum);
+
+    // 节字符串表
+    char *shstrtab = (char *)alloc_kpage(1);
+    Elf32_Shdr *sptr = &shdr[ehdr->e_shstrndx];
+    assert(sptr->sh_type == SHT_STRTAB);
+    assert(sptr->sh_size > 0);
+
+    lseek(fd, sptr->sh_offset, SEEK_SET);
+    read(fd, shstrtab, sptr->sh_size);
+    char *name = shstrtab + 1;
+    while (*name)
+    {
+        LOGK("section name %s\n", name);
+        name += strlen(name) + 1;
+    };
+
+    char *strtab = (char *)alloc_kpage(1);
+
+    sptr = shdr;
+    int strtab_idx = 0;
+    int symtab_idx = 0;
+
+    for (size_t i = 0; i < ehdr->e_shnum; i++)
+    {
+        char *sname = &shstrtab[sptr->sh_name];
+        LOGK("section %s size %d vaddr 0x%p\n",
+             sname, sptr->sh_size, sptr->sh_addr);
+
+        if (!strcmp(sname, ".strtab"))
+            strtab_idx = i;
+        else if (!strcmp(sname, ".symtab"))
+            symtab_idx = i;
+        sptr++;
+    }
+
+    // 程序字符串表
+    sptr = &shdr[strtab_idx];
+    assert(sptr->sh_size > 0);
+    lseek(fd, sptr->sh_offset, SEEK_SET);
+    read(fd, strtab, sptr->sh_size);
+    name = strtab + 1;
+    while (*name)
+    {
+        LOGK("symbol name %s\n", name);
+        name += strlen(name) + 1;
+    };
+
+    // 符号表
+    Elf32_Sym *symtab = (Elf32_Sym *)alloc_kpage(1);
+    sptr = &shdr[symtab_idx];
+    assert(sptr->sh_size > 0);
+    lseek(fd, sptr->sh_offset, SEEK_SET);
+    read(fd, (char *)symtab, sptr->sh_size);
+
+    Elf32_Sym *symptr = symtab;
+    int encount = sptr->sh_size / sptr->sh_entsize;
+    for (size_t i = 0; i < encount; i++)
+    {
+        LOGK("v: 0x%X size: %d sec: %d b: %d t: %d n: %s\n",
+             symptr->st_value,
+             symptr->st_size,
+             symptr->st_shndx,
+             ELF32_ST_BIND(symptr->st_info),
+             ELF32_ST_TYPE(symptr->st_info),
+             &strtab[symptr->st_name]);
+        symptr++;
+    }
+
     free_kpage((u32)ehdr, 1);
     free_kpage((u32)phdr, 1);
+    free_kpage((u32)shdr, 1);
     free_kpage((u32)content, 1);
+    free_kpage((u32)strtab, 1);
+    free_kpage((u32)symtab, 1);
+    free_kpage((u32)shstrtab, 1);
     return 0;
 }
