c81f80d6a20932f4c2de48a2c4c9b6c47a6ec5ae
Steven Kang <kangweibaby@163.com>
2022-12-02 15:39:07 +0800

    🐛 assert #7

--- src/kernel/memory.c
+++ src/kernel/memory.c
@@ -167,8 +167,8 @@ static u32 get_page()
         if (!memory_map[i])
         {
             memory_map[i] = 1;
+            assert(free_pages > 0);
             free_pages--;
-            assert(free_pages >= 0);
             u32 page = PAGE(i);
             LOGK("GET page 0x%p\n", page);
             return page;
