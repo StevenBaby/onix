6fd1c2124028f793e4d5afc58fdf327d3a65d7fd
lihanrui2913 <lihanrui20091103@126.com>
2023-04-03 20:53:27 +0800

    去除了一些没用的分支嵌套

--- src/kernel/acpi.c
+++ src/kernel/acpi.c
@@ -36,12 +36,6 @@ unsigned int *acpi_check_RSDPtr(unsigned int *ptr)
         // found valid rsdpd
         if (check == 0)
         {
-            /*
-             if (desc->Revision == 0)
-               DEBUGK("acpi 1");
-            else
-               DEBUGK("acpi 2");
-            */
             return (unsigned int *)rsdp->RsdtAddress;
         }
     }
@@ -81,68 +75,43 @@ unsigned int *acpi_get_RSDPtr(void)
 // checks for a given header and validates checksum
 int acpi_check_header(unsigned int *ptr, char *sig)
 {
+    char check = 0;
     if (memcmp(ptr, sig, 4) == 0)
     {
         char *checkPtr = (char *)ptr;
         int len = *(ptr + 1);
-        char check = 0;
         while (0 < len--)
         {
             check += *checkPtr;
             checkPtr++;
         }
-        if (check == 0)
-            return 0;
     }
-    return -1;
+    return !(check == 0);
 }
 
 int acpi_enable(void)
 {
     // check if acpi is enabled
-    if ((inw((unsigned int)PM1a_CNT) & SCI_EN) == 0)
+    if ((inw((unsigned int)PM1a_CNT) & SCI_EN) == 0 && SMI_CMD != 0 && ACPI_ENABLE != 0)
     {
         // check if acpi can be enabled
-        if (SMI_CMD != 0 && ACPI_ENABLE != 0)
-        {
-            outb((unsigned int)SMI_CMD, ACPI_ENABLE); // send acpi enable command
-            // give 3 seconds time to enable acpi
-            int i;
-            for (i = 0; i < 300; i++)
+        outb((unsigned int)SMI_CMD, ACPI_ENABLE); // send acpi enable command
+        // give 3 seconds time to enable acpi
+        int i;
+        for (i = 0; i < 300; i++)
+            if ((inw((unsigned int)PM1a_CNT) & SCI_EN) == 1)
+                break;
+
+        if (PM1b_CNT != 0)
+            for (; i < 300; i++)
             {
-                if ((inw((unsigned int)PM1a_CNT) & SCI_EN) == 1)
+                if ((inw((unsigned int)PM1b_CNT) & SCI_EN) == 1)
                     break;
-                sleep(10);
             }
-            if (PM1b_CNT != 0)
-                for (; i < 300; i++)
-                {
-                    if ((inw((unsigned int)PM1b_CNT) & SCI_EN) == 1)
-                        break;
-                    sleep(10);
-                }
-            if (i < 300)
-            {
-                DEBUGK("enabled acpi.\n");
-                return 0;
-            }
-            else
-            {
-                DEBUGK("couldn't enable acpi.\n");
-                return -1;
-            }
-        }
-        else
-        {
-            DEBUGK("no known way to enable acpi.\n");
-            return -1;
-        }
-    }
-    else
-    {
-        // DEBUGK("acpi was already enabled.\n");
+
         return 0;
     }
+    return -1;
 }
 
 int init_acpi(void)
@@ -150,88 +119,59 @@ int init_acpi(void)
     unsigned int *ptr = acpi_get_RSDPtr();
 
     // check if address is correct  ( if acpi is available on this pc )
-    if (ptr != NULL && acpi_check_header(ptr, "RSDT") == 0)
-    {
-        // the RSDT contains an unknown number of pointers to acpi tables
-        int entrys = *(ptr + 1);
-        entrys = (entrys - 36) / 4;
-        ptr += 36 / 4; // skip header information
+    // the RSDT contains an unknown number of pointers to acpi tables
+    int entrys = *(ptr + 1);
+    entrys = (entrys - 36) / 4;
+    ptr += 36 / 4; // skip header information
 
-        while (0 < entrys--)
+    while (entrys-- > 0)
+    {
+        entrys = -2;
+        struct FACP *facp = (struct FACP *)*ptr;
+
+        // search the \_S5 package in the DSDT
+        char *S5Addr = (char *)facp->DSDT + 36; // skip header
+        int dsdtLength = *(facp->DSDT + 1) - 36;
+        while (0 < dsdtLength--)
         {
-            // check if the desired table is reached
-            if (acpi_check_header((unsigned int *)*ptr, "FACP") == 0)
-            {
-                entrys = -2;
-                struct FACP *facp = (struct FACP *)*ptr;
-                if (acpi_check_header((unsigned int *)facp->DSDT, "DSDT") == 0)
-                {
-                    // search the \_S5 package in the DSDT
-                    char *S5Addr = (char *)facp->DSDT + 36; // skip header
-                    int dsdtLength = *(facp->DSDT + 1) - 36;
-                    while (0 < dsdtLength--)
-                    {
-                        if (memcmp(S5Addr, "_S5_", 4) == 0)
-                            break;
-                        S5Addr++;
-                    }
-                    // check if \_S5 was found
-                    if (dsdtLength > 0)
-                    {
-                        // check for valid AML structure
-                        if ((*(S5Addr - 1) == 0x08 || (*(S5Addr - 2) == 0x08 && *(S5Addr - 1) == '\\')) && *(S5Addr + 4) == 0x12)
-                        {
-                            S5Addr += 5;
-                            S5Addr += ((*S5Addr & 0xC0) >> 6) + 2; // calculate PkgLength size
-
-                            if (*S5Addr == 0x0A)
-                                S5Addr++; // skip u8prefix
-                            SLP_TYPa = *(S5Addr) << 10;
-                            S5Addr++;
-
-                            if (*S5Addr == 0x0A)
-                                S5Addr++; // skip u8prefix
-                            SLP_TYPb = *(S5Addr) << 10;
-
-                            SMI_CMD = facp->SMI_CMD;
-
-                            ACPI_ENABLE = facp->ACPI_ENABLE;
-                            ACPI_DISABLE = facp->ACPI_DISABLE;
-
-                            PM1a_CNT = facp->PM1a_CNT_BLK;
-                            PM1b_CNT = facp->PM1b_CNT_BLK;
-
-                            PM1_CNT_LEN = facp->PM1_CNT_LEN;
-
-                            SLP_EN = 1 << 13;
-                            SCI_EN = 1;
-
-                            return 0;
-                        }
-                        else
-                        {
-                            DEBUGK("\\_S5 parse error.\n");
-                        }
-                    }
-                    else
-                    {
-                        DEBUGK("\\_S5 not present.\n");
-                    }
-                }
-                else
-                {
-                    DEBUGK("DSDT invalid.\n");
-                }
-            }
-            ptr++;
+            if (memcmp(S5Addr, "_S5_", 4) == 0)
+                break;
+            S5Addr++;
         }
-        DEBUGK("no valid FACP present.\n");
-    }
-    else
-    {
-        DEBUGK("no acpi.\n");
+        // check for valid AML structure
+        if ((*(S5Addr - 1) == 0x08 || (*(S5Addr - 2) == 0x08 && *(S5Addr - 1) == '\\')) && *(S5Addr + 4) == 0x12)
+        {
+            S5Addr += 5;
+            S5Addr += ((*S5Addr & 0xC0) >> 6) + 2; // calculate PkgLength size
+
+            if (*S5Addr == 0x0A)
+                S5Addr++; // skip u8prefix
+            SLP_TYPa = *(S5Addr) << 10;
+            S5Addr++;
+
+            if (*S5Addr == 0x0A)
+                S5Addr++; // skip u8prefix
+            SLP_TYPb = *(S5Addr) << 10;
+
+            SMI_CMD = facp->SMI_CMD;
+
+            ACPI_ENABLE = facp->ACPI_ENABLE;
+            ACPI_DISABLE = facp->ACPI_DISABLE;
+
+            PM1a_CNT = facp->PM1a_CNT_BLK;
+            PM1b_CNT = facp->PM1b_CNT_BLK;
+
+            PM1_CNT_LEN = facp->PM1_CNT_LEN;
+
+            SLP_EN = 1 << 13;
+            SCI_EN = 1;
+
+            return 0;
+        }
+        ptr++;
     }
 
+    DEBUGK("no valid FACP present.\n");
     return -1;
 }
 
@@ -247,6 +187,4 @@ void acpi_shutdown(void)
     outw((unsigned int)PM1a_CNT, SLP_TYPa | SLP_EN);
     if (PM1b_CNT != 0)
         outw((unsigned int)PM1b_CNT, SLP_TYPb | SLP_EN);
-
-    DEBUGK("acpi poweroff failed.\n");
 }
