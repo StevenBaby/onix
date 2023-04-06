23235f168ed54f10f3fe339684d1e6f01971ef46
Steven Kang <kangweibaby@163.com>
2023-04-06 22:34:45 +0800

    ✨ 130 一些优化 - 输入输出双字

--- src/include/onix/io.h
+++ src/include/onix/io.h
@@ -5,8 +5,10 @@
 
 extern u8 inb(u16 port);  // 输入一个字节
 extern u16 inw(u16 port); // 输入一个字
+extern u32 inl(u16 port); // 输入一个双字
 
 extern void outb(u16 port, u8 value);  // 输出一个字节
 extern void outw(u16 port, u16 value); // 输出一个字
+extern void outl(u16 port, u32 value); // 输出一个双字
 
 #endif
--- src/kernel/io.asm
+++ src/kernel/io.asm
@@ -65,3 +65,36 @@ outw:
 
     leave ; 恢复栈帧
     ret
+
+
+global inl ; 将 inl 导出
+inl:
+    push ebp; 
+    mov ebp, esp ; 保存帧
+
+    xor eax, eax ; 将 eax 清空
+    mov edx, [ebp + 8]; port 
+    in eax, dx; 将端口号 dx 的 32 bit 输入到 eax
+
+    jmp $+2 ; 一点点延迟
+    jmp $+2 ; 一点点延迟
+    jmp $+2 ; 一点点延迟
+
+    leave ; 恢复栈帧
+    ret
+
+global outl
+outl:
+    push ebp; 
+    mov ebp, esp ; 保存帧
+
+    mov edx, [ebp + 8]; port 
+    mov eax, [ebp + 12]; value
+    out dx, eax; 将 eax 中的 32 bit 输入出到 端口号 dx
+
+    jmp $+2 ; 一点点延迟
+    jmp $+2 ; 一点点延迟
+    jmp $+2 ; 一点点延迟
+
+    leave ; 恢复栈帧
+    ret
