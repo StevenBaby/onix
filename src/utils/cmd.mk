3f4008d18d3f4b15ddaa19c0292ae2dfda0876c6
Steven Kang <kangweibaby@163.com>
2023-04-18 00:07:07 +0800

    🎈 docker 开发环境

--- src/utils/cmd.mk
+++ src/utils/cmd.mk
@@ -9,7 +9,7 @@ bochsg: $(IMAGES)
 
 QEMU:= qemu-system-i386 # 虚拟机
 QEMU+= -m 32M # 内存
-QEMU+= -audiodev pa,id=hda # 音频设备
+QEMU+= -audiodev alsa,id=hda # 音频设备
 QEMU+= -machine pcspk-audiodev=hda # pcspeaker 设备
 QEMU+= -rtc base=localtime # 设备本地时间
 QEMU+= -drive file=$(BUILD)/master.img,if=ide,index=0,media=disk,format=raw # 主硬盘
