4c1c85771c96ae4ae3c6380ff343760c3a58f044
rain <20520@126.com>
2023-04-21 13:25:35 +0800

    🐛 libc 构造和析构函数顺序

--- src/lib/crt.asm
+++ src/lib/crt.asm
@@ -17,8 +17,8 @@ _start:
     push eax; 感觉没什么用
     push esp; 用户程序栈最大地址
     push edx; 动态链接器
-    push _init; libc 构造函数
     push _fini; libc 析构函数
+    push _init; libc 构造函数
     push ecx; argv
     push esi; argc
     push main; 主函数
