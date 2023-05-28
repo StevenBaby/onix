edb366148b79ce71608a4bc33396225528b09715
Steven Kang <kangweibaby@163.com>
2023-05-28 15:58:10 +0800

    ✨ 143 基础网络配置 - 打印网络地址

--- src/kernel/test.c
+++ src/kernel/test.c
@@ -8,18 +8,26 @@
 
 err_t sys_test()
 {
-    LOGK("test syscall...\n");
+    // LOGK("test syscall...\n");
 
-    cpu_vendor_t vendor;
+    // cpu_vendor_t vendor;
 
-    cpu_vendor_id(&vendor);
-    printk("CPU vendor id: %s\n", vendor.info);
-    printk("CPU max value: 0x%X\n", vendor.max_value);
+    // cpu_vendor_id(&vendor);
+    // printk("CPU vendor id: %s\n", vendor.info);
+    // printk("CPU max value: 0x%X\n", vendor.max_value);
 
-    cpu_version_t ver;
+    // cpu_version_t ver;
 
-    cpu_version(&ver);
-    printk("FPU support state: %d\n", ver.FPU);
-    printk("APIC support state: %d\n", ver.APIC);
+    // cpu_version(&ver);
+    // printk("FPU support state: %d\n", ver.FPU);
+    // printk("APIC support state: %d\n", ver.APIC);
+
+    // 输出二进制
+    LOGK("%08b\n", 0x5a);
+    // 输出 mac 地址
+    LOGK("%m\n", "\x11\x22\x03\x04\x5f\x5a");
+    // 输出 ip 地址
+    LOGK("%r\n", "\xff\x4e\x03\x04");
+
     return EOK;
 }
--- src/lib/vsprintf.c
+++ src/lib/vsprintf.c
@@ -195,6 +195,7 @@ int vsprintf(char *buf, const char *fmt, va_list args)
     int precision;   // min 整数数字个数；max 字符串中字符个数
     int qualifier;   // 'h', 'l' 或 'L' 用于整数字段
     u32 num;
+    u8 *ptr;
 
     // 首先将字符指针指向 buf
     // 然后扫描格式字符串，
@@ -388,7 +389,34 @@ int vsprintf(char *buf, const char *fmt, va_list args)
             double dnum = va_arg(args, double);
             str = number(str, (u32 *)&dnum, 10, field_width, precision, flags);
             break;
-
+        case 'b': // binary
+            num = va_arg(args, unsigned long);
+            str = number(str, &num, 2, field_width, precision, flags);
+            break;
+        case 'm': // mac address
+            flags |= SMALL | ZEROPAD;
+            ptr = va_arg(args, char *);
+            for (size_t t = 0; t < 6; t++, ptr++)
+            {
+                int num = *ptr;
+                str = number(str, &num, 16, 2, precision, flags);
+                *str = ':';
+                str++;
+            }
+            str--;
+            break;
+        case 'r': // ip address
+            flags |= SMALL;
+            ptr = va_arg(args, u8 *);
+            for (size_t t = 0; t < 4; t++, ptr++)
+            {
+                int num = *ptr;
+                str = number(str, &num, 10, field_width, precision, flags);
+                *str = '.';
+                str++;
+            }
+            str--;
+            break;
         default:
             // 若格式转换符不是 '%'，则表示格式字符串有错
             if (*fmt != '%')
