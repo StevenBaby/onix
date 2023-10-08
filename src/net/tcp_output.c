6f13b29bc28adb0f8f4e8f756e1b6b80c5ef585f
znyin <znyinyyniu@foxmail.com>
2023-10-08 22:21:59 +0800

    🐛 TCP 拆包后数据错误问题 (#48)
    * 🐛 复现TCP拆包后数据错误
    
    * 🐛 修复tcp拆包后数据错误问题
    
    * 🐛 变量命名，代码格式化

--- src/net/tcp_output.c
+++ src/net/tcp_output.c
@@ -47,6 +47,7 @@ err_t tcp_enqueue(tcp_pcb_t *pcb, void *data, size_t size, int flags)
     ip_t *ip;
     tcp_t *tcp;
     pbuf_t *pbuf;
+    void *ptr = data;
 
     if (pcb->snd_mss < TCP_DEFAULT_MSS)
         pcb->snd_mss = TCP_DEFAULT_MSS;
@@ -83,8 +84,11 @@ err_t tcp_enqueue(tcp_pcb_t *pcb, void *data, size_t size, int flags)
         int seglen = (pcb->snd_mss - pbuf->size);
         int len = left < seglen ? left : seglen;
 
-        if (left > 0)
-            memcpy(pbuf->data + pbuf->size, data, len);
+        if (len > 0)
+        {
+            memcpy(pbuf->data + pbuf->size, ptr, len);
+            ptr += len;
+        }
 
         pcb->snd_nbb += len;
         pbuf->size += len;
